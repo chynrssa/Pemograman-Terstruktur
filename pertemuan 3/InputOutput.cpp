6abe2ae130d56590471f28ed40c2538f19541d4c
chynrssa <Cahyaaaaa7@gmail.com>
2024-03-20 17:45:58 +0700

    pertemuan 3
    membahas tentang input dan output.

---
+++ pertemuan 3/InputOutput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    string teks[4];
+
+    for(int i = 0; i < 4; i++){
+        cin >> teks[i];
+    }
+    ofstream writeFile("benda.txt");
+
+    for(int i = 0; i < 4; i++){
+        writeFile << teks[i] << endl;
+    }
+}
