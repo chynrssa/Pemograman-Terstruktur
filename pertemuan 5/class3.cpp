f3b3b4b9cc27a467d7bd13d5284b3d155423a52f
chynrssa <cahyaaaa7@gmail.com>
2024-03-20 10:43:40 +0700

    pertemuan 5
    Mempelajari structure,class,constructor, setter dan getter pada c++

---
+++ pertemuan 5/class3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa{
+	public:
+		string nama,npm;
+		
+		void perkenalan(){
+			cout << "Nama : " << nama << endl;
+			cout << "Npm : " << npm;
+		}
+};
+
+int main(){
+	Mahasiswa Mhs;
+	
+	cin >> Mhs.nama;
+	cin >> Mhs.npm;
+	
+	Mhs.perkenalan();
+}
