af94b96bcfbb6dcba3f0a0b1817cbc60c09beeae
chynrssa <Cahyaaaaa7@gmail.com>
2024-03-20 17:33:23 +0700

    pertemuan 2
    membahas tentang array.

---
+++ pertemuan 1/func1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int pertambahan(int a, int b){
+    return a + b;
+}
+int pengurangan(int a, int b){
+    return a - b;
+}
+int perkalian (int a, int b){
+    return a * b;
+}
+double pembagian(int a, int b){
+    if(b != 0){
+        return a / b;
+    } else {
+        cout << "Pembagian tidak bisa dibagi nol.\n";
+    }
+    
+}
+
+int main(){
+    double x,y;
+    cout << "Masukkan angka pertama : ";
+    cin >> x;
+    cout << "Masukkan angka kdeua : ";
+    cin >> y;
+
+    cout << "=======================================" << endl;
+
+    cout << "Hasil Penjumlahan " << x << " + " << y << " adalah : " << pertambahan(x,y) << "\n";
+    cout << "Hasil Pengurangan " << x << " - " << y << " adalah : " << pengurangan(x,y) << "\n";
+    cout << "Hasil Perkalian " << x << " * " << y << " adalah : " << perkalian(x,y) << "\n";
+    cout << "Hasil Pembagian " << x << " / " << y << " adalah : " << pembagian(x,y) << "\n";
+    
+    return 0;
+}
