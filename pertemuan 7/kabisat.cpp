251e2dac27302b07a1abb0e18dd5f834ae1837ff
chynrssa <Cahyaaaaa7@gmail.com>
2024-03-27 10:16:46 +0700

    Pertemuan 7
    membahasa tentang perhitungan kabisat.

---
+++ pertemuan 7/kabisat.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Perhitungan_Kabisat{
+    private:
+        T* tahun;
+    public:
+        Perhitungan_Kabisat(T* tahun) : tahun (tahun){
+        }
+            T* getTahun() const{
+                return tahun;
+            }
+            void setTahun(T* tahun){
+                this -> tahun = tahun;
+            }
+
+            bool HitungKabisat() const{
+            if(*tahun %4 == 0){
+                if(*tahun % 100 == 0){
+                    if(*tahun % 400 == 0){
+                        return true;
+                    } else{
+                        return false;
+                    }
+                } else{
+                    return true;
+                }
+        } else{
+            return false;
+        }
+    }
+};
+
+int main(){
+    int tahun;
+    cout << "Masukkan tahun : ";
+    cin >> tahun;
+
+    Perhitungan_Kabisat <int> Kabisat(&tahun);
+    if(Kabisat.HitungKabisat()){
+        cout << *Kabisat.getTahun() << " adalah tahun kabisat " << endl;
+    }
+    else{
+        cout << *Kabisat.getTahun() << " bukan tahun kabisat " << endl;
+    }
+    return 0;
+}
