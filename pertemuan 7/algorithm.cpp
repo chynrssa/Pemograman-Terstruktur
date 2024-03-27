e39c1daf68fdaed7bf9094ef3bea7753c6c8cb53
chynrssa <Cahyaaaaa7@gmail.com>
2024-03-27 09:46:19 +0700

    pertemuan 7
    membahas tentang array.

---
+++ pertemuan 7/algorithm.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    vector <int> v;
+    v.push_back(50);
+    
+}
---
+++ pertemuan 7/iterator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    vector <int> v;
+    v.push_back(50);
+
+    vector <int> ::iterator.iter 
+}
